d9aa2d01c771570db39f1a456b4c660eb5abd516
Aryaadkoli <77225698+Aryaadkoli@users.noreply.github.com>
2023-03-26 17:38:37 +0530

    Add files via upload

---
+++ Recursion/Power_of_two.py
@@ -0,0 +1,17 @@
+# Python program to  find and print powers of 2 upto any particular number 'n'.
+# Example: 2^5 = 32.
+
+def powerOfTwo(n):
+    if n == 0:
+        return 1
+    else:
+        power = powerOfTwo(n-1)
+        return power * 2
+
+
+print(powerOfTwo(5))
+
+"""
+The output is as follows:
+32
+"""
